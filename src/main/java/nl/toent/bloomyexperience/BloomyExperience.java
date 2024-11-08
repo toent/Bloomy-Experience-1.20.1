d5789c9d6a1bec7696530b5862805aa21e94667c
Mats <toenterman@gmail.com>
2024-11-08 20:26:53 +0100

    Added random chance for gaining EXP

--- src/main/java/nl/toent/bloomyexperience/BloomyExperience.java
+++ src/main/java/nl/toent/bloomyexperience/BloomyExperience.java
@@ -2,8 +2,33 @@ package nl.toent.bloomyexperience;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.fabricmc.fabric.api.mininglevel.v1.MiningLevelManager;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CommandBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.report.ReporterEnvironment;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ExperienceOrbEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ToolItem;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.Console;
 
 public class BloomyExperience implements ModInitializer {
 	public static final String MOD_ID = "bloomyexperience";
@@ -12,5 +37,59 @@ public class BloomyExperience implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
+
+		PlayerBlockBreakEvents.AFTER.register((world, player, blockPos, blockState, blockEntity) -> {
+			if (!world.isClient && player instanceof ServerPlayerEntity serverPlayer) {
+				int experienceAmount = 1;
+				Block minedBlock = blockState.getBlock();
+
+				if (isCorrectTool(player, blockState)) {
+					serverPlayer.sendMessage(
+							Text.literal("Block: " + minedBlock + " received!" + blockPos).formatted(Formatting.AQUA),
+							false
+					);
+					spawnExp(world, blockPos, experienceAmount);
+				} else {
+					// If the player uses the wrong tool, show a message
+					serverPlayer.sendMessage(
+							Text.literal("You need the correct tool to break this block!").formatted(Formatting.RED),
+							false
+					);
+				}
+
+			}
+		});
+	}
+
+	private boolean isCorrectTool(PlayerEntity player, BlockState blockState) {
+		Item itemInHand = player.getMainHandStack().getItem();
+
+		if (itemInHand instanceof ToolItem tool) {
+			player.sendMessage(
+					Text.literal("block mining level: " + MiningLevelManager.getRequiredMiningLevel(blockState)).formatted(Formatting.GOLD),
+					false
+			);
+
+			player.sendMessage(
+					Text.literal("tool mining level: " + tool.getMaterial().getMiningLevel()).formatted(Formatting.GOLD),
+					false
+			);
+
+			return tool.isSuitableFor(blockState) && MiningLevelManager.getRequiredMiningLevel(blockState) <= tool.getMaterial().getMiningLevel();
+		}
+		return false; // If the item is not a tool, return false
+	}
+
+	private void spawnExp(World world, BlockPos pos, int experienceAmount) {
+//		random maximum
+		int randMax = 10;
+//		random odds 0.5 -> 50% chance
+		float randOdds = 0.2f;
+		float randomNumber = (float) (Math.random() * randMax);
+
+		if(randomNumber > randMax*(1-randOdds)){
+			ExperienceOrbEntity exp = new ExperienceOrbEntity(world, pos.getX(), pos.getY(), pos.getZ(), experienceAmount);
+			world.spawnEntity(exp);
+		}
 	}
 }

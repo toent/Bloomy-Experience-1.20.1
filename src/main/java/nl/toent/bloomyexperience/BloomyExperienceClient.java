603ac101eb04c00561723d3db55f4474b51084b4
Mats <toenterman@gmail.com>
2024-11-08 16:40:31 +0100

    Initial commit

---
+++ src/main/java/nl/toent/bloomyexperience/BloomyExperienceClient.java
@@ -0,0 +1,11 @@
+package nl.toent.bloomyexperience;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BloomyExperienceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+
+}
